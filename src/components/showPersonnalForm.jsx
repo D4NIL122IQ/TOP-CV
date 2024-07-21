868e170a0d178cc90090f1a25fc47ebcd34d9d05
Danil gdj <danilguidjou7@gmail.com>
2024-07-21 21:45:06 +0100

    pour les informations personnels

--- src/components/showPersonnalForm.jsx
+++ src/components/showPersonnalForm.jsx
@@ -1,17 +1,55 @@
 import '../style/generalStyleForForm.css'
+import {useState} from 'react'
 
-export default function AfficherProfil({istrue}) {
-    return istrue && <div className='formu'>
-      <h1>Inforamtion Personnel</h1>
-      <label htmlFor="fname">Nom et Prenom</label>
-      <input type='text' id='fname' value='Hatake Kakashi' onChange={(e)=>{console.log(e.target.value)}}/>
-      <label htmlFor="function">Statue</label>
-      <input type='text' id='function' value='Enseignent' onChange={(e)=>{console.log(e.target.value)}}/>
-      <label htmlFor="phone">Telephone</label>
-      <input type='tel' id='phone' value='+1 123-456-789' onChange={(e)=>{console.log(e.target.value)}}/>
+function AfficherProfil({annuler}) {
+  const [nom, setNom] = useState('Hatake Kakashi');
+  const [statut, setStatut] = useState('Enseignant');
+  const [telephone, setTelephone] = useState('+1 123-456-789');
+  const [email, setEmail] = useState('kakashi@exemple.com');
+  const [adresse, setAdresse] = useState('12 rue je sais pas, konoha');
+
+  return (
+    <div>
+      <h1>Information Personnel</h1>
+      <label htmlFor="fname">Nom et Prénom</label>
+      <input
+        type='text'
+        id='fname'
+        value={nom}
+        onChange={(e) => setNom(e.target.value)}
+      />
+      <label htmlFor="function">Statut</label>
+      <input
+        type='text'
+        id='function'
+        value={statut}
+        onChange={(e) => setStatut(e.target.value)}
+      />
+      <label htmlFor="phone">Téléphone</label>
+      <input
+        type='tel'
+        id='phone'
+        value={telephone}
+        onChange={(e) => setTelephone(e.target.value)}
+      />
       <label htmlFor="mail">E-mail</label>
-      <input type='email' id='mail' value='kakashi@exemple.com' onChange={(e)=>{console.log(e.target.value)}}/>
+      <input
+        type='email'
+        id='mail'
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
       <label htmlFor="adrs">Adresse</label>
-      <input type='text' id='adrs' value='12 rue je sais pas, konoha' onChange={(e)=>{console.log(e.target.value)}}/>
-    </div>
-  }
+      <input
+        type='text'
+        id='adrs'
+        value={adresse}
+        onChange={(e) => setAdresse(e.target.value)}
+      />
+      <input type="button" value="Sauvgarder" />
+      <input type="button" value="Annuler" onClick={()=>{annuler(false)}} />
+    </div>)
+}
+
+
+  export default AfficherProfil
