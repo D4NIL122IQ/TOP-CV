b9b902362de51ab27b1b9e659d27120f391bce3c
Danil gdj <danilguidjou7@gmail.com>
2024-07-24 14:45:35 +0100

    pas encore fini

---
+++ src/components/pdfPreview.jsx
@@ -0,0 +1,15 @@
+import {Document , Page , View, Text} from '@react-pdf/renderer'
+
+function ShowPdfPreviw() {
+    <Document>
+        <Page size='A4'>
+            <View>
+                <Text>
+                    hellow world
+                </Text>
+            </View>
+        </Page>
+    </Document>
+}
+
+export default ShowPdfPreviw
